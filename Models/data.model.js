b4d82de09b45327777fbd8946f86faf233521331
rahulmer82 <rahulmer82@gmail.com>
2024-02-28 22:42:09 +0530

    date time updae

--- Models/data.model.js
+++ Models/data.model.js
@@ -17,32 +17,31 @@ const dataSchema=new mongoose.Schema({
         type:Number,
         default:function(){
             return this.price*this.work
-        },
-        date:{
-            type:String,
-            default: function(){
-                const d = new Date();
-                let date=d.getDate();
-                let month=d.getMonth()+1;
-                let year=d.getFullYear()
-    
-    const dateshow= `${date}/${month}/${year}`;
-    const part=dateshow.split('/')
-    return `${part[0].padStart(2,'0')}/${part[1].padStart(2,'0')}/${part[2]}`
-            }
-        },
-        monthyear:{
-            type:String,
-            default: function(){
-                const date=new Date();
-                const monthName = date.toLocaleString('default', { month: 'long' });
-                const year=date.getFullYear();
-                return `${monthName}-${year}`
-    
-            }
         }
+    },
+    date:{
+        type:String,
+        default: function(){
+            const d = new Date();
+            let date=d.getDate();
+            let month=d.getMonth()+1;
+            let year=d.getFullYear()
 
+const dateshow= `${date}/${month}/${year}`;
+const part=dateshow.split('/')
+return `${part[0].padStart(2,'0')}/${part[1].padStart(2,'0')}/${part[2]}`
+        }
+    },
+    monthyear:{
+        type:String,
+        default: function(){
+            const date=new Date();
+            const monthName = date.toLocaleString('default', { month: 'long' });
+            const year=date.getFullYear();
+            return `${monthName}-${year}`
+
+        }
     }
-},{timestamps:true})
+})
 
 export const UserData=mongoose.model("UserData",dataSchema)
